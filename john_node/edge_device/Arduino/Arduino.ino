bc0a3f58102ede6a05cb735301d85ca07909bf58
John Chung <101233836@students.swinburne.edu.my>
2023-05-18 16:07:38 +0800

    Commit changes from Pi

--- john_node/edge_device/Arduino/Arduino.ino
+++ john_node/edge_device/Arduino/Arduino.ino
@@ -63,6 +63,10 @@ void setup()
   lcd.clear();
 }
 
+void loadInformation() {
+  
+}
+
 void checkButtonClick() {
   for (int i = 0; i < (sizeof(button_status) / sizeof(button_status[0])); i+=2) {
     int reading = digitalRead(button_pins[(i/2)]);
@@ -193,4 +197,3 @@ void loop()
   digitalWrite(FAN, fanStatus);
   noStopDelay(100);
 }
-
