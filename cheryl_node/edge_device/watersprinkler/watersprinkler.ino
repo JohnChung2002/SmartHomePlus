55a569e8db7e50c45093565ad66bd03ae1a450c2
CherylTSW <80512649+CherylTSW@users.noreply.github.com>
2023-05-25 10:18:01 +0800

    Update watersprinkler.ino

--- cheryl_node/edge_device/watersprinkler/watersprinkler.ino
+++ cheryl_node/edge_device/watersprinkler/watersprinkler.ino
@@ -10,14 +10,15 @@ const int waterSensorPin = A1;
 const int ldrPin = A2;    
 
 // LED pin connected to digital pin 12 and 13
-const int ledRed = 13;  
+const int ledRed = 10;  
 const int ledGreen = 12;  
 
 // The number of the pushbutton pin
-const int buttonPin = A3;     
+const int buttonPin = 2;     
 
 // variable for reading the pushbutton status
-int buttonState = 0;         
+int buttonState = 0;    
+int buttonStatus = 0;     
 
 // Temperature range
 const int minTemp = 0;     // Minimum temperature value from the potentiometer
@@ -33,10 +34,15 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);  // Initialize serial communication for debugging
   myServo.attach(11); // Attach the servo to pin 3
+  myServo.write(180); // Starts from this position
 
   // Initialize the LED pin as an output
   pinMode(ledRed, OUTPUT);
   pinMode(ledGreen, OUTPUT);
+
+  // Initialize the LED pin as turned off
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledGreen, LOW);
   
   // Set the LDR pin as an input
   pinMode(ldrPin, INPUT);
@@ -45,7 +51,7 @@ void setup() {
   digitalWrite(ldrPin, LOW);
   
   // Set the button pin as an input
-  pinMode(buttonPin, INPUT); 
+  pinMode(buttonPin, INPUT_PULLUP); 
 
 
 }
@@ -54,6 +60,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     pinStatus = Serial.parseInt();
+    Serial.println(pinStatus);
 
     switch (pinStatus){
       case 1:
@@ -66,18 +73,22 @@ void loop() {
         myServo.write(0);
         break;
       case 4:
+        myServo.write(180);
         digitalWrite(ledGreen, LOW);
         digitalWrite(ledRed, LOW);
         break;
       case 5:
+        myServo.write(90);
         digitalWrite(ledGreen, HIGH);
         digitalWrite(ledRed, HIGH);
         break;
       case 6:
+        myServo.write(90);
         digitalWrite(ledRed, LOW);
         digitalWrite(ledGreen, HIGH);
         break;
       case 7:
+        myServo.write(90);
         digitalWrite(ledRed, HIGH);
         digitalWrite(ledGreen, LOW);
         break;
@@ -91,65 +102,64 @@ void loop() {
 
   // Map the analog value to the temperature range
   int temperature = map(potValue, minTemp, maxTemp, -20, 50);
-//  Serial.print(temperature);
-  // Display weather indication based on temperature value
-  if (temperature < 15) {
-//    Serial.print("Cool weather");  // Display "Cold weather" for temperature below 0
-    Serial.print(temperature);
-  } else {
-//    Serial.print("Hot weather");   // Display "Hot weather" for temperature above 15
-    Serial.print(temperature);
-  }
-  
+  Serial.print(temperature);
+
   Serial.print(",");
+
 
   // Read the analog value from the water sensor
   int wetnessValue = analogRead(waterSensorPin);
-//  Serial.print(wetnessValue);
-  // Display wetness indication based on wetness level
-  if (wetnessValue > threshold ) {
-//    Serial.print("It is currently raining!");    // Display to show if water is detected
-    Serial.print(wetnessValue);
-  } else {
-//    Serial.print("No rain is detected");   // Display to show if no water is detected
-    Serial.print(wetnessValue);
-  }
+  Serial.print(wetnessValue);
+
 
   Serial.print(",");
 
   // Read the analog value from the LDR
   int ldrValue = analogRead(ldrPin);
-//  Serial.println(ldrValue);
-
-  // Check if the LDR value is below a certain threshold, indicating darkness
-  if (ldrValue < 500) {
-    // Turn on the LED
-    digitalWrite(ledRed, HIGH);
-    Serial.println(ldrValue);
-  } else {
-    // Turn off the LED
-    digitalWrite(ledRed, LOW);
-    Serial.println(ldrValue);
-  }
-
+  Serial.println(ldrValue);
 //  Serial.print(",");
 
+
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+//  buttonPress();
+//  Serial.print("Button State: ");
+//  Serial.println(buttonState);
 
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(13, HIGH);
+//  if (buttonState == LOW) {
+//    // turn LED on:
+//    digitalWrite(10, HIGH);
 //    Serial.println(buttonState);
-  } else {
-    // turn LED off:
-    digitalWrite(13, LOW);    
+//  } else {
+//    // turn LED off:
+//    digitalWrite(10, LOW);    
 //    Serial.println(buttonState);
-  }
+//  }
   
-  delay(10000);  // Delay for stability
+  delay(1000);  // Delay for stability
 
 
+}
+
+void buttonPress() {
+  int reading = digitalRead(buttonPin);
+  if (reading == 0 && buttonStatus == 0) {
+    buttonState = !buttonState;
+    //Status Changed
+    buttonStatus = 1;
+  }
+  if (reading == 1 && buttonStatus == 1) {
+    buttonStatus = 0;
+  }
+}
+
+
+void stableDelay(int interval) {
+  long startDelayTime = millis();
+  while (true) {
+    if (millis() - startDelayTime >= interval) {
+      return;
+    }
+  }
 }
