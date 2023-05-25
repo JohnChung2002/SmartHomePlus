3141cdf3561ddc84bb9be73c6aa93aa4654d7b29
John Chung <101233836@students.swinburne.edu.my>
2023-05-25 08:10:11 +0800

    John webpage

--- main/blueprint.py
+++ main/blueprint.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from .components.authentication import auth_bp
+from .components.home_remote import home_remote_bp
 
 bp = Blueprint("main", __name__)
 
 bp.register_blueprint(auth_bp)
+bp.register_blueprint(home_remote_bp)
