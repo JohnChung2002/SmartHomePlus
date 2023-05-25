579d626d1f0fa6b5dc2eddd3f97346e8902629ed
CherylTSW <80512649+CherylTSW@users.noreply.github.com>
2023-05-25 10:14:36 +0800

    Upload flask file

---
+++ cheryl_node/WaterSprinkler.py
@@ -0,0 +1,108 @@
+import mysql.connector
+import serial 
+import time 
+from flask import Blueprint, Flask, render_template, redirect, url_for, request
+
+def connect_db():
+    return mysql.connector.connect(user='hp', password='0123', host='localhost', database='waterSprinkler_db')
+
+ 
+sprinkler_bp = Blueprint('WaterSprinkler', __name__)
+
+@sprinkler_bp.route('/WaterSprinkler', methods=['POST'])
+
+# Dictionary of pins with name of pin and state ON/OFF 
+pins = { 
+        11: {'name' : 'PIN 11', 'state' : 0}  
+        } 
+
+def display_data():
+    # Connect to the database
+    db = connect_db()
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the SQL query to retrieve data from the database
+    cur.execute("SELECT * FROM systemData ORDER BY dataID DESC LIMIT 20")
+
+    # Fetch all the rows returned by the query
+    rows = cur.fetchall()
+    print(rows[0])
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+    
+    return rows
+
+# Main function when accessing the website 
+@sprinkler_bp.route("/") 
+def index():  
+    # This data will be sent to index.html (pin dictionary)
+    rows = display_data()
+    templateData = { 
+        'pins' : pins,
+        'rows' : rows
+    } 
+    # Pass the template data into the template index.html and return it 
+    return render_template('index.html', **templateData)
+
+ 
+# Function to send simple commands 
+@sprinkler_bp.route("/<action>") 
+def action(action): 
+    if action == 'action1' : 
+#         ser.write(b"1")
+        client.publish(topic, "Sprinkler Off")
+        pins[11]['state'] = 0 
+    if action == 'action2' : 
+#         ser.write(b"2")
+        client.publish(topic, "Sprinkler On")
+        pins[11]['state'] = 1
+    if action == 'action3' : 
+#         ser.write(b"3")
+        client.publish(topic, "Spray at intruder")
+        pins[11]['state'] = 2
+#     if action == 'action4' : 
+#         ser.write(b"4") 
+#         pins[12]['state'] = 0
+#     if action == 'action5' : 
+#         ser.write(b"5") 
+#         pins[12]['state'] = 1
+        
+    return redirect(url_for('index'))
+
+#  This is to change the threshold value of the brightness
+@sprinkler_bp.route('/submit-form', methods=['POST'])
+def submit_form():
+    wetnessVal = int(request.form['wetnessVal'])
+    # Connect to the database
+    db = connect_db()
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the SQL query to retrieve data from the database
+    sql = "UPDATE settingTable SET wetnessValue = %s WHERE tableID = 1"
+    values = (wetnessVal,)
+    print(wetnessVal)
+    
+    cur.execute(sql, values)
+    
+    # Commit the changes to the database
+    db.commit()
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+#     return 'Form submitted successfully'
+    return redirect(url_for('index'))
+
+     
+# Main function, set up serial bus, indicate port for the webserver, and start the service 
+if __name__ == "__main__" : 
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) 
+    ser.flush()
+    sprinkler_bp.run(host='0.0.0.0', port = 8080, debug = False) 
+
