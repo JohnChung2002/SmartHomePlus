2e5ef2478473f32692b91c575f15ff29436aa98e
John Chung <101233836@students.swinburne.edu.my>
2023-05-27 09:53:12 +0800

    Edge changes

--- john_node/edge_device/edge.py
+++ john_node/edge_device/edge.py
@@ -94,6 +94,16 @@ def on_message(client, userdata, msg):
                     mysql.update("appliance_status", ["status"], ["appliance_id"], [json_message["status"], aircon_dict[json_message["room"]]])
                 elif json_message["title"] == "Aircon Temp":
                     mysql.update("appliance_status", ["status_value"], ["appliance_id"], [json_message["temp"], aircon_dict[json_message["room"]]])
+        if (json_message["title"] in ["Lights", "Aircon Switch", "Ventilating Fan"]):
+            appliance_id = lights_dict[json_message["room"]] if json_message["title"] == "Lights" else aircon_dict[json_message["room"]] if json_message["title"] == "Aircon Switch" else 6
+            mysql.get_by_id("appliance_uptime", ["appliance_id"], [int(appliance_id)])
+            message = {
+                "title": "Update Uptime",
+                "sender": "Edge",
+                "appliance_id": appliance_id,
+                "uptime": mysql.data["uptime"]
+            }
+            client.publish(topic, json.dumps(message))
         print("Received message: ", msg.payload.decode())
     
 client = mqtt.Client()
