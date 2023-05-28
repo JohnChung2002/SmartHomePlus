a55d6350544c766e1017473b28a689e398e5f0e9
John Chung <101233836@students.swinburne.edu.my>
2023-05-28 12:20:29 +0800

    Website overhaul

--- main/components/dashboard.py
+++ main/components/dashboard.py
@@ -4,6 +4,13 @@ from shared.services.validation_service import validate_login
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
+@dashboard_bp.route('/', methods=['GET'])
+@auth_middleware
+def dashboard():
+    with g.dbconn:
+        user = g.dbconn.get_by_id("user_details", ["user_id"], [session["user_id"]])
+    return render_template('index.html', name=user["name"])
+
 @dashboard_bp.route('/home_control', methods=['GET'])
 @auth_middleware
 def home_remote():
